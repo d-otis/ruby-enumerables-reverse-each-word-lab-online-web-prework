bd2c4fa57c3c5bc1aa1d4ce17bf53d955b0392d8
Dan Foley <dan@dan-foley.com>
2019-12-18 00:41:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   arr = string.split(/ /)
   result = ""
   arr.each do |word|
-     result += word.reverse + " "
+     result += word.reverse + " ".trim
      p result
   end
 end
